d1fe44abbfe8b7f2d0874dc47957f531ef964c41
Michał Woźniak <mimowo@users.noreply.github.com>
2026-04-17 16:43:04 +0200

    Fix e2e helper waiting for NodeReady after disruption (#10580)

--- test/util/util.go
+++ test/util/util.go
@@ -1504,6 +1504,7 @@ func waitForDummyWorkloadToRunOnNode(ctx context.Context, c client.Client, node 
 			Queue(kueue.LocalQueueName(lq.Name)).
 			NodeSelector(corev1.LabelHostname, node.Name).
 			Image(GetAgnHostImage(), BehaviorExitFast).
+			RequestAndLimit(corev1.ResourceCPU, "200m").
 			Obj()
 
 		MustCreate(ctx, c, dummyJob)
@@ -1515,7 +1516,7 @@ func waitForDummyWorkloadToRunOnNode(ctx context.Context, c client.Client, node 
 				Type:   batchv1.JobComplete,
 				Status: corev1.ConditionTrue,
 			}, cmpopts.IgnoreFields(batchv1.JobCondition{}, "LastTransitionTime", "LastProbeTime", "Reason", "Message"))))
-		}, MediumTimeout, Interval).Should(gomega.Succeed())
+		}, LongTimeout, Interval).Should(gomega.Succeed())
 	})
 }
 
